89ddabb9d93c420a01256a3bbd590066ad64e8f6
wangmin23 <wangmin23@meituan.com>
2018-02-03 21:58:10 +0800

    add: 变量学习

---
+++ sh/variable/string.sh
@@ -0,0 +1,20 @@
+# #! /bin/bash
+# # 单引号
+# name='my name is qianlongo'
+# echo $name
+# # 双引号
+# name2="$name"
+# echo $name2
+# str=$name
+# # 可以像js的模板字符串一样操作
+# echo "${str}"
+# echo ${str}
+
+# 提取长度
+# name='qianlongo'
+# len=${#name}
+# echo $len
+# echo ${name:0:len-1}
+# 查找子字符串
+name="runoob is a great company"
+echo "expr index $name is"  # 输出 8
---
+++ sh/variable/test1.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# 定义变量 左右不能有空格 可以被重写
+# name=qianlongo
+# echo $name
+# name=huihui
+# echo $name
+
+# 只读变量
+# myName=qianlongo
+# readOnly myName
+# myName=huihui
+
+# 删除变量
+# mySex=boy
+# unset mySex
+# echo $mySex
